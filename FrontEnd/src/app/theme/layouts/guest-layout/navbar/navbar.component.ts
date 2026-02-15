41026223ecb4c23234012025bf871c0ed2cd8543
hamza znaidi <hamzaznaidi539@gmail.com>
2026-02-15 20:08:59 +0100

    initial commit

---
+++ FrontEnd/src/app/theme/layouts/guest-layout/navbar/navbar.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { AuthService } from '../../../../core/services/auth.service';
+
+@Component({
+  selector: 'app-navbar',
+  imports: [CommonModule, RouterModule, MatToolbarModule, MatButtonModule, MatIconModule],
+  templateUrl: './navbar.component.html',
+  styleUrl: './navbar.component.scss',
+})
+export class NavbarComponent {
+  menuOpen = false;
+
+  constructor(public auth: AuthService) { }
+
+  toggleMenu(): void {
+    this.menuOpen = !this.menuOpen;
+  }
+}
